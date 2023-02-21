ef496ac80a8051cdf8b251b0afeb5150cb64753c
huangzhangshu <huangzhangshu@infracreate.com>
2023-02-21 12:57:43 +0800

    chore: fix utils.sh

--- .github/utils/utils.sh
+++ .github/utils/utils.sh
@@ -5,6 +5,9 @@ set -eo pipefail
 images=$1
 images_list=$2
 
+rm -f $images_list
+touch $images_list
+
 for image in `echo "$images" | sed 's/|/ /g'`; do
-   echo "$image" > $images_list
+   echo "$image" >> $images_list
 done
