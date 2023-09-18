649a6159ac05a4b5bd0c52a33459746ccfe84dd2
huangzhangshu <huangzhangshu@apecloud.com>
2023-09-18 22:28:29 +0800

    chore: Skipping success

--- .github/utils/skopeo_copy_to_aliyun.sh
+++ .github/utils/skopeo_copy_to_aliyun.sh
@@ -12,7 +12,8 @@ fi
 while read -r image
 do
     image_name=${image##*/}
-    echo "skopeo copy $REGISTRY/$image to registry.cn-hangzhou.aliyuncs.com/apecloud/$image_name"
+    skopeo_msg="skopeo copy $REGISTRY/$image to registry.cn-hangzhou.aliyuncs.com/apecloud/$image_name"
+    echo "$skopeo_msg"
     skopeo_flag=0
     for i in {1..10}; do
         ret_msg=$(skopeo copy --all \
@@ -22,14 +23,14 @@ do
             docker://registry.cn-hangzhou.aliyuncs.com/apecloud/$image_name)
         echo "return message:$ret_msg"
         if [[ "$ret_msg" == *"Storing list signatures"* || "$ret_msg" == *"Skipping"* ]]; then
-            echo "$(tput -T xterm setaf 2)$ret_msg$(tput -T xterm sgr0)"
+            echo "$(tput -T xterm setaf 2)$skopeo_msg success$(tput -T xterm sgr0)"
             skopeo_flag=1
             break
         fi
         sleep 1
     done
     if [[ $skopeo_flag -eq 0 ]]; then
-        echo "$(tput -T xterm setaf 1)skopeo copy $REGISTRY/$image to registry.cn-hangzhou.aliyuncs.com/apecloud/$image_name error$(tput -T xterm sgr0)"
+        echo "$(tput -T xterm setaf 1)$skopeo_msg error$(tput -T xterm sgr0)"
         exit 1
     fi
 done < $FILE_NAME
--- .github/utils/skopeo_sync_to_aliyun_new.sh
+++ .github/utils/skopeo_sync_to_aliyun_new.sh
@@ -13,7 +13,8 @@ fi
 
 while read -r image
 do
-    echo "skopeo sync $REGISTRY/$image to infracreate-registry.cn-zhangjiakou.cr.aliyuncs.com/apecloud"
+    skopeo_msg="skopeo sync $REGISTRY/$image to infracreate-registry.cn-zhangjiakou.cr.aliyuncs.com/apecloud"
+    echo "$skopeo_msg"
     skopeo_flag=0
     for i in {1..10}; do
         ret_msg=$( skopeo sync --all \
@@ -27,14 +28,14 @@ do
             infracreate-registry.cn-zhangjiakou.cr.aliyuncs.com/apecloud)
         echo "return message:$ret_msg"
         if [[ "$ret_msg" == *"Storing list signatures"* || "$ret_msg" == *"Skipping"* ]]; then
-            echo "$(tput -T xterm setaf 2)$ret_msg$(tput -T xterm sgr0)"
+            echo "$(tput -T xterm setaf 2)$skopeo_msg success$(tput -T xterm sgr0)"
             skopeo_flag=1
             break
         fi
         sleep 1
     done
     if [[ $skopeo_flag -eq 0 ]]; then
-        echo "$(tput -T xterm setaf 1)skopeo sync $REGISTRY/$image to registry.cn-hangzhou.aliyuncs.com/apecloud error$(tput -T xterm sgr0)"
+        echo "$(tput -T xterm setaf 1)$skopeo_msg error$(tput -T xterm sgr0)"
         exit 1
     fi
 done < $FILE_NAME
